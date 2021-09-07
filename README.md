ac2ddc0a6fb32335a19757b160211235771de83d
araceli <carmona.araceli.4690@gmail.com>
2021-09-06 21:10:15 -0700

    deploying

---
+++ README.md
@@ -0,0 +1,24 @@
+# React Portfolio
+This is a single-page application portfolio displaying my recent web-development projects. When viewers load the portfolio they are presented with a page containing a header, a section for content, and a footer. The header contains an about me section, a contact section, a portfolio section, and a resume section. When viewers click on any of the navigation titles then they are redirected to the according section. This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). 
+
+## Table of Contents
+- [Technologies](#Technologies)
+- [Running-the-App](#Running-the-App)
+- [Deployed-App](#Deployed-app)
+- [Contact](#Contact)
+
+## Technologies 
+- React
+- Bootstrap
+- JSX
+
+## Running-the-App
+To see the application in action you can clone the repo and install the appropriate packages. Run 'npm start' in your terminal to run the app in development mode. Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits. You will also see any lint erros in the console.
+
+## Deployed-App
+You can also view the application at the following [github](https://araceli4690.github.io/react-portfolio/) link. The homepage that displays will look like the one below.
+
+![](./src/assets/cover/portfolio.png)
+
+## Contact
+If you have any questions feel free to contact me through my [Github](https://github.com/Araceli4690).
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,14 @@ function About() {
             <div className="mx-2">
                 <br />
                 <p>
-                    Experienced Research Assistant with a demonstrated history of working in the higher education industry. Skilled in front-end and back-end web development as well as data analysis. Strong research professional with a Bachelor's degree focused in Linguistics from California State University, Long Beach turned full-stack web developer. I am a problem-solver and critical thinker who is always eager to learn new skills and technologies. My experience working in research labs has turned me into an excellent team player and organizer with great communication skills. Did I also mention that I am an expert at googling?
+                    Experienced Research Assistant with a demonstrated history of working in the higher
+                    education industry. Skilled in front-end and back-end web development as well as data
+                    analysis. Strong research professional with a Bachelor's degree focused in Linguistics
+                    from California State University, Long Beach turned full-stack web developer. I am a
+                    problem-solver and critical thinker who is always eager to learn new skills and
+                    technologies. My experience working in research labs has turned me into an excellent
+                    team player and organizer with great communication skills. Did I also mention that I am
+                    an expert at googling?
                 </p>
                 <br />
             </div>
